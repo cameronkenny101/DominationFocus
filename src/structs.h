d396fb3f4ae92d99f03e8f484086dbc56ec9f974
cameronkenny101 <55995394+cameronkenny101@users.noreply.github.com>
2020-04-13 22:09:01 +0100

    bugs

--- src/structs.h
+++ src/structs.h
@@ -5,21 +5,25 @@
 #ifndef DOMINATIONGAME_STRUCTS_H
 #define DOMINATIONGAME_STRUCTS_H
 
+// struct for color type
 typedef enum color {
     RED,
     GREEN
 }color;
 
+// struct for square type
 typedef enum square_type {
     VALID,
     INVALID
 }square_type;
 
+// struct for pieces color and pointers to each piece
 typedef struct piece {
     color p_color;
     struct piece * next;
 }piece;
 
+// struct for square, the number of pieces on a square, pointer to piece top of square and pointer to piece bottom of square
 typedef struct square {
     square_type type;
     piece * stack;
@@ -27,6 +31,7 @@ typedef struct square {
     int num_pieces;
 }square;
 
+// struct for player, there name, color, pieces captured and pieces destroyed
 typedef struct player {
     color player_color;
     char player_name[30];
